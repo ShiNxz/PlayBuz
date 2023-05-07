ccf0058db539c4d905b2728f37f3eb24c6fa476e
Amir Eliz <amir.eliz155@gmail.com>
2023-05-08 00:22:19 +0300

    ...

--- app/(main)/store/[gameId]/head.tsx
+++ app/(main)/store/[gameId]/head.tsx
@@ -1,1 +1,18 @@
 // generate title and description for each game
+
+import games from '@/data/games'
+
+export async function generateMetadata({ params }: IProps) {
+	const game = games.find((g) => g.id === params.gameId)
+
+	return {
+		title: `PlayBuz | ${game?.name || ''}`,
+		description: game?.description || '',
+	}
+}
+
+interface IProps {
+	params: {
+		gameId: string
+	}
+}

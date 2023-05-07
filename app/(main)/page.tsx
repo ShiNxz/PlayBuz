12dd4a3cac303d06e2ab9154ac2351d4fbe31c0e
Amir Eliz <amir.eliz155@gmail.com>
2023-05-08 00:29:10 +0300

    ...

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,11 +1,14 @@
 'use client'
 
 import { useRouter } from 'next/navigation'
+import { useEffect } from 'react'
 
 const HomePage = () => {
 	const router = useRouter()
 
-	router.push('/store')
+	useEffect(() => {
+		router.push('/store')
+	}, [router])
 
 	return <></>
 }
